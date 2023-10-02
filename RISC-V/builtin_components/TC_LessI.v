0eb800c678cc0d4ed1aa5131657f662b31991aa6
Lv Yitian <15988566900@qq.com>
2023-10-02 23:41:07 +0800

    Add files via upload

---
+++ RISC-V/builtin_components/TC_LessI.v
@@ -0,0 +1,10 @@
+module TC_LessI(in0, in1, out);
+    parameter UUID = 0;
+    parameter NAME = "";
+    parameter BIT_WIDTH = 1;
+    input signed [BIT_WIDTH-1:0] in0;
+    input signed [BIT_WIDTH-1:0] in1;
+    output out;
+    
+    assign out = in0 < in1;
+endmodule
