7f9f6e2367d8e724e1b90ee989ec7081484dc4c0
Lv Yitian <15988566900@qq.com>
2023-11-14 11:43:54 +0800

    Add files via upload

--- StandardVariation.java
+++ StandardVariation.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.function.Function:
+import java.util.function.Function;
 import java.util.Arrays;
 public class StandardVariation{
     public static double sum(double[] arr){
