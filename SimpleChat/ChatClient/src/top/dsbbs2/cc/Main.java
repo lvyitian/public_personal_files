5516e1406738c5074238d580784903fa41e02449
Lv Yitian <15988566900@qq.com>
2024-06-12 17:02:07 +0800

    Add files via upload

--- SimpleChat/ChatClient/src/top/dsbbs2/cc/Main.java
+++ SimpleChat/ChatClient/src/top/dsbbs2/cc/Main.java
@@ -1,7 +1,28 @@
 package top.dsbbs2.cc;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
 public class Main {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Throwable {
+		Terminal terminal=TerminalBuilder.builder().build();
+		terminal.echo(true);
+		System.setIn(terminal.input());
+		System.setOut(new PrintStream(terminal.output(),true,terminal.encoding().name()));
+		System.setErr(new PrintStream(terminal.output(),true,terminal.encoding().name()));
+		new Thread(()->{
+			while(true){
+				System.out.println(1);
+				try{
+					Thread.sleep(100);
+				}catch(Throwable t){throw new RuntimeException(t);}
+			}
+		}){{this.setDaemon(true);}}.start();
+		try(Scanner input=new Scanner(terminal.reader())){
+			input.nextLine();
+		}
 	}
 }
